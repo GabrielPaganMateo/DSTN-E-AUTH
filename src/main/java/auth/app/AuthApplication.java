35b648cf2b3ca574e6c5fca5dbff02ced35452a4
GabrielPaganMateo <gabriel.pagan.mateo@gmail.com>
2024-03-24 09:31:43 -0300

    Create SpringBoot Entry Point

---
+++ src/main/java/auth/app/AuthApplication.java
@@ -0,0 +1,13 @@
+package auth.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
