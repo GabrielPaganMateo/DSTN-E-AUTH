f7c9195e461192a4cf77502001e0db55e3992d37
GabrielPaganMateo <gabriel.pagan.mateo@gmail.com>
2024-03-25 15:49:35 -0300

    Add User Entity and PSQL DB connection

---
+++ src/main/java/auth/app/entities/User.java
@@ -0,0 +1,24 @@
+package auth.app.entities;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private UUID id;
+	private String userName;
+	private String password;
+}
