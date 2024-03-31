ab5c5b6b428d08e3aecc78e8d85dcf44dfda8da4
GabrielPaganMateo <gabriel.pagan.mateo@gmail.com>
2024-03-30 23:28:38 -0300

    Add requests to postman and fix secret key for symmetric hash

--- src/main/java/auth/app/interfase/impl/JwtGeneratorImpl.java
+++ src/main/java/auth/app/interfase/impl/JwtGeneratorImpl.java
@@ -9,16 +9,14 @@ import org.springframework.stereotype.Service;
 
 import auth.app.entity.EndUser;
 import auth.app.interfase.JwtGenerator;
-import auth.app.security.SecretGenerator;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class JwtGeneratorImpl implements JwtGenerator {
-	/**
+
 	@Value("${jwt.secret}")
 	private String secret;
-	*/
 	
 	@Value("${jwt.message}")
 	private String message;
@@ -26,7 +24,7 @@ public class JwtGeneratorImpl implements JwtGenerator {
 	@Override
 	public Map<String, String> generateJwt(EndUser user) {
 		String token = "";
-		token = Jwts.builder().setSubject(user.getUserName()).setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, SecretGenerator.generateSecret(32)).compact();
+		token = Jwts.builder().setSubject(user.getId().toString()).setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, secret).compact();
 		Map<String, String> jwt = new HashMap<>();
 		System.out.println(user.getId());
 		jwt.put("token", token);
